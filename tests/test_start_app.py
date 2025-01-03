b1918842312b219c23866a4c3ae3c4ff1f1a02e2
meleksabit <angel3.85bg@gmail.com>
2025-01-03 03:41:09 +0200

    edit mock testing

--- tests/test_start_app.py
+++ tests/test_start_app.py
@@ -1,9 +1,9 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import my_app.start_app
+import pytest
 
 @patch("prometheus_client.start_http_server")
 def test_start_prometheus(mock_start_http):
-    """Test Prometheus server start."""
-    my_app.start_app.start_prometheus()
+    from my_app.start_app import start_prometheus
+    start_prometheus()
     mock_start_http.assert_called_once_with(8000)
-    
